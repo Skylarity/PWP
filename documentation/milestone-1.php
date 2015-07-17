d12222a1097463733dc309615d3128a39d23c6a0
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-16 21:54:41 -0600

    Expanded on M1 documentation

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -5,8 +5,34 @@
 		<title></title>
 	</head>
 	<body>
+
+		<h1>
+			Personal Website Project
+		</h1>
+
+		<!-- Purpose -->
+		<h2>
+			Purpose:
+		</h2>
 		<p>
-			Milestone 1.
+			This is the purpose.
 		</p>
+
+		<!-- Audience -->
+		<h2>
+			Audience:
+		</h2>
+		<p>
+			This is the audience.
+		</p>
+
+		<!-- Goal -->
+		<h2>
+			Goal:
+		</h2>
+		<p>
+			This is the goal.
+		</p>
+
 	</body>
 </html>
