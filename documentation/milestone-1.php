e9983890e83259155c3e9fbae56ccedcb3f17064
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-22 10:31:58 -0600

    Outlined purpose, audience, and goal

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -7,31 +7,35 @@
 	<body>
 
 		<h1>
-			Personal Website Project
+			Personal Website Project: Milestone 1
 		</h1>
 
 		<!-- Purpose -->
 		<h2>
 			Purpose:
 		</h2>
+
 		<p>
-			This is the purpose.
+			THe purpose of this website will be to act as a portfolio for all of my different works.
 		</p>
 
 		<!-- Audience -->
 		<h2>
 			Audience:
 		</h2>
+
 		<p>
-			This is the audience.
+			The audience of this website will be employers and/or customers.
 		</p>
 
 		<!-- Goal -->
 		<h2>
 			Goal:
 		</h2>
+
 		<p>
-			This is the goal.
+			The goal of this website will be to elegantly and clearly convey all of my skills and prior works, and
+			attract potential employers and customers.
 		</p>
 
 	</body>
