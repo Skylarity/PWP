0fb7f79dea6952c2393509dc2caba421e5c9a403
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-22 11:55:41 -0600

    Created style for documentation

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -2,41 +2,74 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/documentation.css">
+
+		<!-- Google Fonts -->
+		<link
+			href='https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic,100,100italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700'
+			rel='stylesheet' type='text/css'>
+
+		<title>PWP Docs - Milestone 1</title>
 	</head>
 	<body>
 
-		<h1>
-			Personal Website Project: Milestone 1
-		</h1>
+		<div class="global">
 
-		<!-- Purpose -->
-		<h2>
-			Purpose:
-		</h2>
+			<header class="title">
+				<h1 class="container">
+					Personal Website Project: <span class="thin-title">Milestone 1</span>
+				</h1>
+			</header>
 
-		<p>
-			THe purpose of this website will be to act as a portfolio for all of my different works.
-		</p>
+			<!-- Purpose -->
+			<div id="purpose" class="full-width padding-top-50">
+				<div class="container">
+					<h2 class="thin">
+						Purpose:
+					</h2>
 
-		<!-- Audience -->
-		<h2>
-			Audience:
-		</h2>
+					<p>
+						The purpose of this website will be to act as a portfolio for all of the content I've created.
+					</p>
 
-		<p>
-			The audience of this website will be employers and/or customers.
-		</p>
+					<div class="hr"></div>
+				</div>
+			</div>
 
-		<!-- Goal -->
-		<h2>
-			Goal:
-		</h2>
+			<!-- Audience -->
+			<div id="audience" class="full-width">
+				<div class="container">
+					<h2 class="thin">
+						Audience:
+					</h2>
 
-		<p>
-			The goal of this website will be to elegantly and clearly convey all of my skills and prior works, and
-			attract potential employers and customers.
-		</p>
+					<p>
+						The audience of this website will be employers and/or customers.
+					</p>
+
+					<div class="hr"></div>
+				</div>
+			</div>
+
+			<!-- Goal -->
+			<div id="goal" class="full-width">
+				<div class="container">
+					<h2 class="thin">
+						Goal:
+					</h2>
+
+					<p>
+						The goal of this website will be to elegantly and clearly convey all of my skills and prior
+						works,
+						and
+						attract potential employers and customers.
+					</p>
+				</div>
+			</div>
+
+		</div>
 
 	</body>
 </html>
