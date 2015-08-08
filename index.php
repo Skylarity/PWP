85de97194dc71b2de975fc8022573cbb6742a3b0
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-07 22:48:33 -0600

    Realized mock-up main page

--- index.php
+++ index.php
@@ -1,25 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title></title>
-	</head>
-	<body>
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<h1>
-			Personal Website Project
-		</h1>
+        <!-- Bootstrap Latest compiled and minified CSS -->
+        <link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
 
-		<h2>
-			Documentation:
-		</h2>
-		<ol>
-			<li>
-				<a href="documentation/milestone-1.php">Milestone 1</a>
-			</li>
-			<li>
-				<a href="documentation/milestone-2.php">Milestone 2</a>
-			</li>
-		</ol>
-	</body>
+        <!-- Optional Bootstrap theme -->
+        <link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+              rel="stylesheet"/>
+
+        <!-- Custom CSS -->
+        <link type="text/css" href="css/style.css" rel="stylesheet"/>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script type="text/javascript"
+                src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+        <script type="text/javascript"
+                src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+        <script type="text/javascript"
+                src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+        <!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+        <!-- FontAwesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+        <!-- Google Webfonts -->
+        <link
+            href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic'
+            rel='stylesheet' type='text/css'>
+
+        <title>Skyler Rexroad - Web Developer - Musician</title>
+    </head>
+    <body class="sfooter">
+        <div class="sfooter-content">
+            <header>
+                <div class="container">
+                    <img class="logo" src="images/robot-white-inverted.png"/>
+
+                    <h1>SKYLARITY - <span class="thin">SKYLER REXROAD</span></h1>
+                    <ul class="nav navbar-nav pull-right">
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-home"></i>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-laptop"></i>
+                                Websites</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-headphones"></i>
+                                Music
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-envelope"></i>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+
+            <!-- Body -->
+
+            <!-- Main call to action -->
+            <div class="bg-image">
+                <div class="container">
+                    <div class="main-title">
+                        <h2 class="white-text center main-text">
+                            <span class="thin smaller">YOUR CONTENT.</span>
+                            <br/>
+                            DONE WELL.
+                        </h2>
+                        <a class="btn btn-large call-to-action" href="#">LET'S GO</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Mission Statement -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h2>
+                            Mission Statement
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-9">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi
+                            ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                            esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                            in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div class="col-md-3 align-right">
+                        <img src="https://placehold.it/200x150">
+                    </div>
+                </div>
+            </div>
+
+            <!-- How I work -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9"></div>
+                    <div class="col-md-3">
+                        <h2 class="align-right">
+                            How I work
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="https://placehold.it/200x150">
+                    </div>
+                    <div class="col-md-9">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi
+                            ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                            esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                            in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <span class="larger">
+                            <strong>CONTACT: </strong>
+                            <a class="email" href="mailto:skyler@skylarity.com">skyler@skylarity.com</a>
+                        </span>
+                    </div>
+                    <div class="col-md-9">
+                        <ul class="nav nav-pills pull-right">
+                            <li>
+                                <a href="https://twitter.com/Skylarity" target="_blank">
+                                    <i class="fa fa-twitter"></i>
+                                    Twitter
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://soundcloud.com/skylarity" target="_blank">
+                                    <i class="fa fa-soundcloud"></i>
+                                    SoundCloud
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/skylerrexroad" target="_blank">
+                                    <i class="fa fa-linkedin"></i>
+                                    LinkedIn
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
 </html>
